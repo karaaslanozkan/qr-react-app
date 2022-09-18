374ef28aa728a8b038b532b02deb9f51ec47848b
unknown <karaaslanozkan@hotmail.com>
2022-09-18 19:34:21 +0300

    initial commit

--- src/Qr.js
+++ src/Qr.js
@@ -2,38 +2,39 @@ import React, { useState, useEffect } from 'react';
 import useQrReader from 'react-qr-reader';
 import './style.css';
 import axios from 'axios';
-export default function Qr() {
-  const [result, setResult] = useState(
-    'https://jsonplaceholder.typicode.com/users/3'
-  );
-  const [product, setProduct] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  let handleScan = (data) => {
-    if (data) {
-      axios(data)
-      .then((res) => setProduct(res.data))
-      .catch((e) => console.log(e))
-      .finally(() => setIsLoading(false));
-    }
-  };
-
-  let handleError = (err) => {
-    // alert(err);
-  };
+function Test() {
+    const [result, setResult] = useState(
+        'https://jsonplaceholder.typicode.com/users/3'
+      );
+      const [product, setProduct] = useState([]);
+      const [isLoading, setIsLoading] = useState(true);
+      let handleScan = (data) => {
+        if (data) {
+          axios(data)
+          .then((res) => setProduct(res.data))
+          .catch((e) => console.log(e))
+          .finally(() => setIsLoading(false));
+        }
+      };
+    
+      let handleError = (err) => {
+        // alert(err);
+      };
   return (
     <div>
-      <useQrReader
-        delay={300}
-        onError={handleError}
-        onScan={handleScan}
-        style={{ width: '100%' }}
-        facingMode="environment"
-      />
-      <p>
-        {isLoading && <div>Yükleniyor</div>}
-        Ürünün Fiyatı : {product.id}
-      </p>
+    <useQrReader
+      delay={300}
+      onError={handleError}
+      onScan={handleScan}
+      style={{ width: '100%' }}
+      facingMode="environment"
+    />
+    <p>
+      {isLoading && <div>Yükleniyor</div>}
+      Ürünün Fiyatı : {product.id}
+    </p>
 </div>
-);
+  )
 }
+
+export default Test
