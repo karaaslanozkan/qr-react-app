3a3b14bba071402eadb8d6fa54b96b57c3c7635f
unknown <karaaslanozkan@hotmail.com>
2022-09-18 19:54:33 +0300

    initial commit

--- src/Qr.js
+++ src/Qr.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import QrReader from 'react-qr-reader';
 import './style.css';
 import axios from 'axios';
-function Test() {
+function Qr() {
     const [result, setResult] = useState(
         'https://jsonplaceholder.typicode.com/users/3'
       );
@@ -37,4 +37,4 @@ function Test() {
   )
 }
 
-export default Test
+export default Qr;
