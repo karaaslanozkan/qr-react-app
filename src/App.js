68a068acd560a72a16c539cb13f0e76ad206676c
unknown <karaaslanozkan@hotmail.com>
2022-09-18 19:51:07 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,39 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import QrReader from 'react-qr-reader';
-import './style.css';
-import axios from 'axios';
-export default function App() {
-  const [result, setResult] = useState(
-    'https://jsonplaceholder.typicode.com/users/3'
-  );
-  const [product, setProduct] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  let handleScan = (data) => {
-    if (data) {
-      axios(data)
-      .then((res) => setProduct(res.data))
-      .catch((e) => console.log(e))
-      .finally(() => setIsLoading(false));
-    }
-  };
-
-  let handleError = (err) => {
-    // alert(err);
-  };
+import './App.css';
+import Qr from './Qr.js';
+function App() {
   return (
-    <div>
-      <QrReader
-        delay={300}
-        onError={handleError}
-        onScan={handleScan}
-        style={{ width: '100%' }}
-        facingMode="environment"
-      />
-      <p>
-        {isLoading && <div>Yükleniyor</div>}
-        Ürünün Fiyatı : {product.id}
-      </p>
-</div>
-);
+    <div className="App">
+      <header className="App-header">
+<Qr></Qr>
+      </header>
+    </div>
+  );
 }
+
+export default App;
