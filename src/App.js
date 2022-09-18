11bd33cb87069c47b22ddd08f0d979900f752188
unknown <karaaslanozkan@hotmail.com>
2022-09-18 18:37:06 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import useQrReader from 'react-qr-reader';
+import QrReader from 'react-qr-reader';
 import './style.css';
 import axios from 'axios';
 export default function App() {
@@ -23,7 +23,7 @@ export default function App() {
   };
   return (
     <div>
-      <useQrReader
+      <QrReader
         delay={300}
         onError={handleError}
         onScan={handleScan}
