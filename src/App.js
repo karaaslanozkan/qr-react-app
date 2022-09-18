05aa30a48da5d3a74b68f3f8c37bdf572a6637e5
unknown <karaaslanozkan@hotmail.com>
2022-09-18 19:25:32 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-
+import Qr from './Qr.js';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+<Qr></Qr>
       </header>
     </div>
   );
